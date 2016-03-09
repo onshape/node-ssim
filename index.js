60f1165a4d48c199d6098bcaae4086efe5ca4be6
Mark Barr <mbarr@onshape.com>
2016-03-09 16:01:36 -0500

    Clean ups and image difference improvements.

--- index.js
+++ index.js
@@ -28,11 +28,15 @@ function SSIM() {
   };
 
   self.generateDifference = function(image1, image2, outputFileName, callback) {
-    var difference  = new Buffer(image1.width * image1.height * image1.channels);
+    var total = image1.height * image1.width * 4;
+    var difference  = new Buffer(total);
 
-    var offset = 0;
-    for(var y = 0; y < image1.height; y++) {
-      for(var x = 0; x < image1.width; x++) {
+    var ae = [ 0, 0, 0, 0];
+    var mae = [ 0, 0, 0, 0 ];
+    var fuzz = 64; // absolute color channel distance
+
+    for(var offset = 0; offset < total; offset += 4) {
+
         var red1 = image1.data[offset];
         var green1 = image1.data[offset + 1];
         var blue1 = image1.data[offset + 2];
@@ -43,62 +47,105 @@ function SSIM() {
         var blue2 = image2.data[offset + 2];
         var alpha2 = image2.data[offset + 3];
 
-        if (red1 !== red2 || green1 !== green2 || blue1 !== blue2) {
-          difference[offset] = 255;
-          difference[offset + 1] = 0;
-          difference[offset + 2] = 0;
-          difference[offset + 3] = 255;
-        } else {
+        var distances = [ Math.abs(red2 - red1),
+                          Math.abs(green2 - green1),
+                          Math.abs(blue2 - blue1),
+                          Math.abs(alpha2 - alpha1) ];
+
+        var match = true;
+        distances.forEach(function(dist) {
+          if (dist > fuzz) {
+            match = false;
+          }
+        });
+
+        if (match) {
           difference[offset] = red1;
           difference[offset + 1] = green1;
           difference[offset + 2] = blue1;
           difference[offset + 3] = 100;
-        }
-        offset += 4;
-      }
-    }
-    var png = new PNG();
-    png.width = image1.width;
-    png.height = image1.height;
-    png.data = difference;
+        } else {
+          difference[offset] = 255;
+          difference[offset + 1] = 0;
+          difference[offset + 2] = 0;
+          difference[offset + 3] = 255;
 
-    png.pack().pipe(fs.createWriteStream(outputFileName)).on('close', function() {
-      callback(difference);
-    });
-  }
+          ae[0] += distances[0];
+          ae[1] += distances[1];
+          ae[2] += distances[2];
+          ae[3] += distances[3];
+        }
+    }
+
+    mae[0] = ae[0] / total;
+    mae[1] = ae[1] / total;
+    mae[2] = ae[2] / total;
+    mae[3] = ae[3] / total;
+
+    var result = { ae: ae, mae: mae };
+
+    if (outputFileName) {
+      var png = new PNG();
+      png.width = image1.width;
+      png.height = image1.height;
+      png.data = difference;
+
+      png.pack().pipe(fs.createWriteStream(outputFileName)).on('close', function() {
+        callback(result);
+      });
+    } else {
+      callback(result);
+    }
+  };
 
   self.compare = function(image1, image2, windowSize, K1, K2, luminance, bitsPerComponent) {
-    if (windowSize === void 0) { windowSize = 8; }
-    if (K1 === void 0) { K1 = 0.01; }
-    if (K2 === void 0) { K2 = 0.03; }
-    if (luminance === void 0) { luminance = true; }
-    if (bitsPerComponent === void 0) { bitsPerComponent = 8; }
+    if (windowSize === undefined) {
+      windowSize = 64; // default 8 x 8 window size
+    }
+    if (K1 === undefined) {
+      K1 = 0.01; // default k1 of 0.01
+    }
+    if (K2 === undefined) {
+      K2 = 0.03; // default k2 of 0.03
+    }
+    if (luminance === undefined) {
+      luminance = true;
+    }
+    if (bitsPerComponent === undefined) {
+      bitsPerComponent = 8;
+    }
     if (image1.width !== image2.width || image1.height !== image2.height) {
-      return 0; // Not similar
+      return { ssim: 0, mcs: 0 };
     }
 
-    /* tslint:disable:no-bitwise */
     var L = (1 << bitsPerComponent) - 1;
-    /* tslint:enable:no-bitwise */
-    var c1 = Math.pow((K1 * L), 2), c2 = Math.pow((K2 * L), 2), numWindows = 0, mssim = 0.0;
+    var c1 = Math.pow((K1 * L), 2);
+    var c2 = Math.pow((K2 * L), 2);
+    var numWindows = 0;
+    var mssim = 0.0;
     var mcs = 0.0;
 
     function iteration(lumaValues1, lumaValues2, averageLumaValue1, averageLumaValue2) {
       // calculate variance and covariance
       var sigxy, sigsqx, sigsqy;
       sigxy = sigsqx = sigsqy = 0.0;
+
       for (var i = 0; i < lumaValues1.length; i++) {
         sigsqx += Math.pow((lumaValues1[i] - averageLumaValue1), 2);
         sigsqy += Math.pow((lumaValues2[i] - averageLumaValue2), 2);
         sigxy += (lumaValues1[i] - averageLumaValue1) * (lumaValues2[i] - averageLumaValue2);
       }
+
       var numPixelsInWin = lumaValues1.length - 1;
+
       sigsqx /= numPixelsInWin;
       sigsqy /= numPixelsInWin;
       sigxy /= numPixelsInWin;
+
       // perform ssim calculation on window
       var numerator = (2 * averageLumaValue1 * averageLumaValue2 + c1) * (2 * sigxy + c2);
       var denominator = (Math.pow(averageLumaValue1, 2) + Math.pow(averageLumaValue2, 2) + c1) * (sigsqx + sigsqy + c2);
+
       mssim += numerator / denominator;
       mcs += (2 * sigxy + c2) / (sigsqx + sigsqy + c2);
       numWindows++;
@@ -115,19 +162,26 @@ function SSIM() {
       for (var x = 0; x < width; x += windowSize) {
         // avoid out-of-width/height
         var windowWidth = Math.min(windowSize, width - x), windowHeight = Math.min(windowSize, height - y);
-        var lumaValues1 = _lumaValuesForWindow(image1, x, y, windowWidth, windowHeight, luminance), lumaValues2 = _lumaValuesForWindow(image2, x, y, windowWidth, windowHeight, luminance), averageLuma1 = _averageLuma(lumaValues1), averageLuma2 = _averageLuma(lumaValues2);
+        var lumaValues1 = _lumaValuesForWindow(image1, x, y, windowWidth, windowHeight, luminance);
+        var lumaValues2 = _lumaValuesForWindow(image2, x, y, windowWidth, windowHeight, luminance);
+        var averageLuma1 = _averageLuma(lumaValues1);
+        var averageLuma2 = _averageLuma(lumaValues2);
         callback(lumaValues1, lumaValues2, averageLuma1, averageLuma2);
       }
     }
   }
 
   function _lumaValuesForWindow(image, x, y, width, height, luminance) {
-    var array = image.data, lumaValues = new Float32Array(new ArrayBuffer(width * height * 4)), counter = 0;
+    var array = image.data;
+    var lumaValues = new Float32Array(new ArrayBuffer(width * height * 4));
+    var counter = 0;
     var maxj = y + height;
+
     for (var j = y; j < maxj; j++) {
       var offset = j * image.width;
       var i = (offset + x) * image.channels;
       var maxi = (offset + x + width) * image.channels;
+
       switch (image.channels) {
       case 1 /* Grey */:
         while (i < maxi) {
@@ -180,15 +234,13 @@ function SSIM() {
   self.calculateSSIM = function(imageFileA, imageFileB, outputFileName, callback) {
     self.loadImage(imageFileA, function(imageA) {
       self.loadImage(imageFileB, function(imageB) {
-        if (outputFileName) {
-          self.generateDifference(imageA, imageB, outputFileName, function() {
-            callback(self.compare(imageA, imageB));
-          });
-        } else {
-          callback(self.compare(imageA, imageB));
-        }
+        self.generateDifference(imageA, imageB, outputFileName, function(difference) {
+          var cmp = self.compare(imageA, imageB);
+          callback({ ssim: cmp.ssim, mcs: cmp.mcs, mae: difference.mae, ae: difference.ae });
+        });
       });
     });
   };
 };
+
 module.exports = new SSIM();
